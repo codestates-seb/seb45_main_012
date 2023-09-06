e9dfa94912305091b66710353860b1c3b11ab648
박지민 <jumpm9239@gmail.com>
2023-09-06 11:16:46 +0900

    chore: merge 충돌 해결4

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
+import Header from './components/header';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
     </div>
   );
 }
-
-export default App;
